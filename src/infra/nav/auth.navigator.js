2b2f537cfc4ed2a7f41b8ec9f226bb3640914e66
Rational Chestnut <brian.kw.mason@gmail.com>
2024-09-21 19:20:08 -0400

    Got rid of bad learning screen

--- src/infra/nav/auth.navigator.js
+++ src/infra/nav/auth.navigator.js
@@ -3,12 +3,9 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import MapScreen from "../../pages/Home/Home.page";
 import ProfileScreen from "../../pages/Profile/ProfileScreen.page";
-import LearnScreen from "../../pages/Learn/LearnScreen.page";
-import SkillDetailScreen from "../../pages/SkillDetail/SkillDetailScreen.page"; // Import the new screen for skill details
 import { FontAwesome } from "@expo/vector-icons";
 import { Summary } from "../../pages/Summary/Summary.page";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
-import { RFPercentage } from "react-native-responsive-fontsize";
 import { Chat } from "../../pages/Chat/Chat.page";
 
 const Stack = createStackNavigator();
@@ -22,50 +19,6 @@ const HomeNavigator = () => {
       <Stack.Screen name="Map" component={MapScreen} />
       <Stack.Screen name="Summary" component={Summary} />
     </Stack.Navigator>
-  );
-};
-
-// Stack Navigator for Learn Screen and Skill Detail
-const LearnStack = () => {
-  return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="LearnScreen" component={LearnScreen} />
-      <Stack.Screen name="SkillDetail" component={SkillDetailScreen} />
-    </Stack.Navigator>
-  );
-};
-
-// Top Tab Navigator for Learn Stack
-const LearnNavigator = () => {
-  return (
-    <TopTab.Navigator
-      screenOptions={{
-        headerShown: false,
-        swipeEnabled: true,
-        tabBarActiveTintColor: "white",
-        tabBarLabelStyle: {
-          textTransform: "capitalize",
-          fontSize: RFPercentage(2.5),
-        },
-        tabBarStyle: {
-          backgroundColor: "white",
-        },
-        tabBarIndicatorStyle: {
-          backgroundColor: "#4CAF50",
-        },
-      }}
-    >
-      <TopTab.Screen
-        name="Chat"
-        component={Chat}
-        options={{ tabBarLabel: "Chat" }} // Add a custom label for the tab
-      />
-      <TopTab.Screen
-        name="LearnStack"
-        component={LearnStack}
-        options={{ tabBarLabel: "Learn" }} // Add a custom label for the tab
-      />
-    </TopTab.Navigator>
   );
 };
 
@@ -99,7 +52,7 @@ export const AuthenticatedNavigator = () => {
       }}
     >
       <Tab.Screen name="Home" component={HomeNavigator} />
-      <Tab.Screen name="Learn" component={LearnNavigator} />
+      <Tab.Screen name="Learn" component={Chat} />
       <Tab.Screen name="Profile" component={ProfileScreen} />
     </Tab.Navigator>
   );
